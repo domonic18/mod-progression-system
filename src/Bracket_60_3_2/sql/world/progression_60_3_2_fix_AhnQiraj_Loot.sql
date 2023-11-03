17cc6326f05e9594829986a08eb6c89be07df2ee
Deawalk <zgdmemail@gmail.com>
2023-11-03 21:38:56 +0800

    Fix：增加安其拉废墟无疤者的掉落

---
+++ src/Bracket_60_3_2/sql/world/progression_60_3_2_fix_AhnQiraj_Loot.sql
@@ -0,0 +1,10 @@
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 15339);
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(15339, 20884, 0, 0, 0, 1, 1, 1, 1, 'Ossirian the Unscarred - Qiraji Magisterial Ring'),
+(15339, 20886, 0, 0, 0, 1, 2, 1, 1, 'Ossirian the Unscarred - Qiraji Spiked Hilt'),
+(15339, 20888, 0, 0, 0, 1, 1, 1, 1, 'Ossirian the Unscarred - Qiraji Ceremonial Ring'),
+(15339, 20890, 0, 0, 0, 1, 2, 1, 1, 'Ossirian the Unscarred - Qiraji Ornate Hilt'),
+(15339, 21220, 0, 100, 0, 1, 0, 1, 1, 'Ossirian the Unscarred - Head of Ossirian the Unscarred'),
+(15339, 34024, 34024, 100, 0, 1, 3, 1, 2, 'Ossirian the Unscarred - (ReferenceTable)'),
+(15339, 34025, 34025, 100, 0, 1, 4, 1, 2, 'Ossirian the Unscarred - (ReferenceTable)'),
+(15339, 190024, 34024, 1, 0, 1, 1, 1, 1, '');
---
+++ src/Bracket_60_3_4/sql/world/progression_60_3_4_naxx_loot_del_Emblem.sql
@@ -0,0 +1,39 @@
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 15956) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 15936) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 15930) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 15932) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 15931) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 15953) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 16061) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 15990) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 16064) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 16011) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 15952) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 16065) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 16028) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 30549) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 15989) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 16063) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 16060) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 15929) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 15928) AND (`Item` IN (47241));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 15954) AND (`Item` IN (47241));
