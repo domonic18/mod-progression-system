68450fae0d5664492c80f1af040a956d82217925
Deawalk <zgdmemail@gmail.com>
2025-01-26 14:21:02 +0800

    修改毒蛇神殿Boss的血量为1.5倍

---
+++ src/Bracket_70_3_1/sql/world/progression_61_64_boss_health.sql
@@ -0,0 +1,20 @@
+-- Hydross the Unstable（HealthModifier=315）
+UPDATE `creature_template` SET `HealthModifier` = 473 WHERE (`entry` = 21216);
+
+-- Leotheras the Blind（HealthModifier=350）
+UPDATE `creature_template` SET `HealthModifier` = 525 WHERE (`entry` = 21215);
+
+-- Morogrim Tidewalker（HealthModifier=525）
+UPDATE `creature_template` SET `HealthModifier` = 788 WHERE (`entry` = 21213);
+
+-- Fathom-Lord Karathress（HealthModifier=210）
+UPDATE `creature_template` SET `HealthModifier` = 315 WHERE (`entry` = 21214);
+
+-- Lady Vashj（HealthModifier=560）
+UPDATE `creature_template` SET `HealthModifier` = 840 WHERE (`entry` = 21212);
+
+-- The Lurker Below（HealthModifier=350）
+UPDATE `creature_template` SET `HealthModifier` = 525 WHERE (`entry` = 21217);
+
+-- Toxic Sporebat（HealthModifier=0.7）
+UPDATE `creature_template` SET `HealthModifier` = 1 WHERE (`entry` = 22140);
