997beb52035ec1b324fdd26b2566d506d2c998a7
Deawalk <zgdmemail@gmail.com>
2025-03-01 13:47:42 +0800

    添加zUL'man的boss_heath调整

---
+++ src/Bracket_70_5/sql/world/progression_70_5_boss_health.sql
@@ -0,0 +1,17 @@
+-- Akil'zon（HealthModifier=154）
+UPDATE `creature_template` SET `HealthModifier` = 231 WHERE (`entry` = 23574);
+
+-- Halazzi（HealthModifier=56.875）
+UPDATE `creature_template` SET `HealthModifier` = 85 WHERE (`entry` = 23577);
+
+-- Jan'alai（HealthModifier=94.5）
+UPDATE `creature_template` SET `HealthModifier` = 142 WHERE (`entry` = 23578);
+
+-- Nalorakk（HealthModifier=126.35）
+UPDATE `creature_template` SET `HealthModifier` = 190 WHERE (`entry` = 23576);
+
+-- Hex Lord Malacrass（HealthModifier=94.5）
+UPDATE `creature_template` SET `HealthModifier` = 142 WHERE (`entry` = 24239);
+
+-- Zul'jin（HealthModifier=157.5）
+UPDATE `creature_template` SET `HealthModifier` = 236 WHERE (`entry` = 23863);
