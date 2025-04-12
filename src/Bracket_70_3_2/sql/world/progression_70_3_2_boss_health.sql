d7ec92f6c7a55be2740bfe102f5f26b7c9ccc93b
Deawalk <zgdmemail@gmail.com>
2025-04-12 10:36:37 +0800

    添加风暴要塞血量提升2倍的配置

---
+++ src/Bracket_70_3_2/sql/world/progression_70_3_2_boss_health.sql
@@ -0,0 +1,83 @@
+-- Al'ar（HealthModifier=280 → 560)
+UPDATE `creature_template` SET `HealthModifier` = 560 WHERE (`entry` = 19514);
+
+-- High Astromancer Solarian（HealthModifier=350 → 700) 
+UPDATE `creature_template` SET `HealthModifier` = 700 WHERE (`entry` = 18805);
+
+-- Void Reaver（HealthModifier=525 → 1050)
+UPDATE `creature_template` SET `HealthModifier` = 1050 WHERE (`entry` = 19516);
+
+-- Kael'thas Sunstrider（HealthModifier=420 → 840)
+UPDATE `creature_template` SET `HealthModifier` = 840 WHERE (`entry` = 19622);
+
+-- Bloodwarder Legionnaire（HealthModifier=17.5 → 35)
+UPDATE `creature_template` SET `HealthModifier` = 35 WHERE (`entry` = 20031);
+
+-- Bloodwarder Vindicator（HealthModifier=28 → 56)
+UPDATE `creature_template` SET `HealthModifier` = 56 WHERE (`entry` = 20032);
+
+-- Astromancer（HealthModifier=17.5 → 35)
+UPDATE `creature_template` SET `HealthModifier` = 35 WHERE (`entry` = 20033);
+
+-- Star Scryer（HealthModifier=17.5 → 35)
+UPDATE `creature_template` SET `HealthModifier` = 35 WHERE (`entry` = 20034);
+
+-- Bloodwarder Marshal（HealthModifier=28 → 56)
+UPDATE `creature_template` SET `HealthModifier` = 56 WHERE (`entry` = 20035);
+
+-- Bloodwarder Squire（HealthModifier=17.5 → 35)
+UPDATE `creature_template` SET `HealthModifier` = 35 WHERE (`entry` = 20036);
+
+-- Tempest Falconer（HealthModifier=17.5 → 35)
+UPDATE `creature_template` SET `HealthModifier` = 35 WHERE (`entry` = 20037);
+
+-- Phoenix-Hawk Hatchling（HealthModifier=7 → 14)
+UPDATE `creature_template` SET `HealthModifier` = 14 WHERE (`entry` = 20038);
+
+-- Phoenix-Hawk（HealthModifier=52.5 → 105)
+UPDATE `creature_template` SET `HealthModifier` = 105 WHERE (`entry` = 20039);
+
+-- Crystalcore Devastator（HealthModifier=52.5 → 105)
+UPDATE `creature_template` SET `HealthModifier` = 105 WHERE (`entry` = 20040);
+
+-- Crystalcore Sentinel（HealthModifier=28 → 56)
+UPDATE `creature_template` SET `HealthModifier` = 56 WHERE (`entry` = 20041);
+
+-- Tempest-Smith（HealthModifier=17.5 → 35)
+UPDATE `creature_template` SET `HealthModifier` = 35 WHERE (`entry` = 20042);
+
+-- Apprentice Star Scryer（HealthModifier=2.8 → 5.6)
+UPDATE `creature_template` SET `HealthModifier` = 5.6 WHERE (`entry` = 20043);
+
+-- Novice Astromancer（HealthModifier=2.8 → 5.6)
+UPDATE `creature_template` SET `HealthModifier` = 5.6 WHERE (`entry` = 20044);
+
+-- Nether Scryer（HealthModifier=28 → 56)
+UPDATE `creature_template` SET `HealthModifier` = 56 WHERE (`entry` = 20045);
+
+-- Astromancer Lord（HealthModifier=28 → 56)
+UPDATE `creature_template` SET `HealthModifier` = 56 WHERE (`entry` = 20046);
+
+-- Crimson Hand Battle Mage（HealthModifier=17.5 → 35)
+UPDATE `creature_template` SET `HealthModifier` = 35 WHERE (`entry` = 20047);
+
+-- Crimson Hand Centurion（HealthModifier=17.5 → 35)
+UPDATE `creature_template` SET `HealthModifier` = 35 WHERE (`entry` = 20048);
+
+-- Crimson Hand Blood Knight（HealthModifier=28 → 56)
+UPDATE `creature_template` SET `HealthModifier` = 56 WHERE (`entry` = 20049);
+
+-- Crimson Hand Inquisitor（HealthModifier=28 → 56)
+UPDATE `creature_template` SET `HealthModifier` = 56 WHERE (`entry` = 20050);
+
+-- Lord Sanguinar（HealthModifier=25.2 → 50.4)
+UPDATE `creature_template` SET `HealthModifier` = 50.4 WHERE (`entry` = 20060);
+
+-- Grand Astromancer Capernian（HealthModifier=25.2 → 50.4)
+UPDATE `creature_template` SET `HealthModifier` = 50.4 WHERE (`entry` = 20062);
+
+-- Master Engineer Telonicus（HealthModifier=25.2 → 50.4)
+UPDATE `creature_template` SET `HealthModifier` = 50.4 WHERE (`entry` = 20063);
+
+-- Thaladred the Darkener（HealthModifier=25.2 → 50.4)
+UPDATE `creature_template` SET `HealthModifier` = 50.4 WHERE (`entry` = 20064);
